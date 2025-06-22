d71fe7b9399aa3871fa9ed8b8659e830595b510f
Student-STUDENTED <mahadikreshma408@.gmail.com>
2025-06-22 21:56:36 +0530

    p

---
+++ src/PatternPrograms/ReverseRightHalfPyramid.java
@@ -0,0 +1,17 @@
+package PatternPrograms;
+
+public class ReverseRightHalfPyramid {
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 0; i < n ; i++) {
+
+            for (int j = 1; j <= n-i ; j++) {
+
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/PatternPrograms/SquareFillPattern.java
@@ -0,0 +1,18 @@
+package PatternPrograms;
+
+public class SquareFillPattern {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < n ; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
